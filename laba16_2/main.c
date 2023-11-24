91e5017451c56d491bd30855158df8a9aeede036
zorklin <well.well.well.pink@gmail.com>
2023-11-24 10:13:37 +0200

    new_veersion

--- laba16_2/main.c
+++ laba16_2/main.c
@@ -3,24 +3,21 @@
 void main() {
 
 	//типізація, масив, для циклів і допоміжні зміні для пошуку номера рядку і суми рядку
-	int i = 0, j = 0, height, width, sum_of_string = 0, number_of_string = 0, numerosity_of_min_number = 1;
+	int i = 0, j = 0, height_width, sum_of_string = 0, number_of_string = 0, numerosity_of_min_number = 1;
 	float array[10][10];
 
-	// введення розміру масиву (висота)
-	printf("enter height of array, a positive integer is more than 0 and less than 11: ");
-	scanf_s("%d", &height);
+	// введення розміру масиву (висота і ширина)
+	printf("enter height-width of array, a positive integer is more than 0 and less than 11: ");
+	scanf_s("%d", &height_width);
 
-	// введення розміру масиву (ширина)
-	printf("enter width of array, a positive integer is more than 0 and less than 11: ");
-	scanf_s("%d", &width);
 
 	//перевірка чи коректні значення (вихід за масив)
-	if (0 < height && height < 11 && 0 < width && width < 11) 
+	if (0 < height_width && height_width < 11)
 	{
 		//ведення елементів масиву
-		for (i = 0; i < height; i++)
+		for (i = 0; i < height_width; i++)
 		{
-			for (j = 0; j < width; j++)
+			for (j = 0; j < height_width; j++)
 			{
 				printf("Element %d %d: ", (i + 1), (j + 1));
 				scanf_s("%f", &array[i][j]);
@@ -32,10 +29,10 @@ void main() {
 		int min_number_of_array = array[0][0];
 
 		//пошук рядка з мінімальним значенням
-		for (i = 0; i < height; i++)
+		for (i = 0; i < height_width; i++)
 		{
 
-			for (j = 0; j < width; j++)
+			for (j = 0; j < height_width; j++)
 			{
 				//якщо у рядку знайдено елемент елемент менший за найманще значення
 				if (array[i][j] < min_number_of_array)
@@ -68,7 +65,7 @@ void main() {
 		{
 
 			//знаходження суми для цього рядка
-			for (i = 0; i < width; i++)
+			for (i = 0; i < height_width; i++)
 			{
 				sum_of_string += array[number_of_string][i];
 			}
